68a6c3d2406d81970c65f659298d560f702a7174
MattMistele <matthew@mistele.com>
2018-03-04 01:20:54 -0800

    alkdsjf

--- backend1.php
+++ backend1.php
@@ -2,7 +2,7 @@
 
 echo "Hello World "
 
- //require("phpsqlajax_dbinfo.php");
+ require("phpsqlajax_dbinfo.php");
 
  $servername="disaster-database-mysql-azure.mysql.database.azure.com";
  $username="myadmin@disaster-database-mysql-azure";
@@ -22,42 +22,42 @@ return $xmlStr;
 // Opens a connection to a MySQL server
 $connection=mysql_connect ($servername, $username, $password);
 
-if (!$connection) {
-  die('Not connected : ' . mysql_error());
-}
+// if (!$connection) {
+//   die('Not connected : ' . mysql_error());
+// }
 
-// Set the active MySQL database
-$db_selected = mysql_select_db($database, $connection);
-if (!$db_selected) {
-  die ('Can\'t use db : ' . mysql_error());
-}
+// // Set the active MySQL database
+// $db_selected = mysql_select_db($database, $connection);
+// if (!$db_selected) {
+//   die ('Can\'t use db : ' . mysql_error());
+// }
 
-// Select all the rows in the markers table
-$query = "SELECT * FROM markers WHERE 1";
-$result = mysql_query($query);
-if (!$result) {
-  die('Invalid query: ' . mysql_error());
-}
+// // Select all the rows in the markers table
+// $query = "SELECT * FROM markers WHERE 1";
+// $result = mysql_query($query);
+// if (!$result) {
+//   die('Invalid query: ' . mysql_error());
+// }
 
-header("Content-type: text/xml");
+// header("Content-type: text/xml");
 
-// Start XML file, echo parent node
-echo '<markers>';
+// // Start XML file, echo parent node
+// echo '<markers>';
 
-// Iterate through the rows, printing XML nodes for each
-while ($row = @mysql_fetch_assoc($result)){
-  // Add to XML document node
-  echo '<marker ';
-  echo 'id="' . $ind . '" ';
-  echo 'name="' . parseToXML($row['name']) . '" ';
-  echo 'address="' . parseToXML($row['address']) . '" ';
-  echo 'lat="' . $row['lat'] . '" ';
-  echo 'lng="' . $row['lng'] . '" ';
-  echo 'type="' . $row['type'] . '" ';
-  echo '/>';
-}
+// // Iterate through the rows, printing XML nodes for each
+// while ($row = @mysql_fetch_assoc($result)){
+//   // Add to XML document node
+//   echo '<marker ';
+//   echo 'id="' . $ind . '" ';
+//   echo 'name="' . parseToXML($row['name']) . '" ';
+//   echo 'address="' . parseToXML($row['address']) . '" ';
+//   echo 'lat="' . $row['lat'] . '" ';
+//   echo 'lng="' . $row['lng'] . '" ';
+//   echo 'type="' . $row['type'] . '" ';
+//   echo '/>';
+// }
 
-// End XML file
-echo '</markers>';
+// // End XML file
+// echo '</markers>';
 
 ?>
